fe5300c3c45948d953630418c78bf7dcf9a0cffe
안병진 <zmd9220@gmail.com>
2021-05-20 16:43:05 +0900

    210520 modeling and movies index page

--- server/movies/views.py
+++ server/movies/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Movie
+from .serializer import MovieSerializer
 # Create your views here.
+
+@api_view(['GET'])
+def movies(request):
+    movie_list = get_list_or_404(Movie)
+    serializer = MovieSerializer(movie_list, many=True)
+    return Response(serializer.data)
