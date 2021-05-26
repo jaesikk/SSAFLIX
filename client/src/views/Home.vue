09af1fe75d42071402123808b7786dcf5473f405
엄재식 <jasic0627@gmail.com>
2021-05-26 10:25:11 +0900

    0526_ReviewDetail

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -31,12 +31,17 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">영화제목</h5>
+            <h5 class="modal-title" id="staticBackdropLabel">{{ selectMovie.title}}</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body">
-            <p>%%%%OVERVIEW%%%</p>
-          </div>
+            <div class="modal-body">
+              <p>{{ selectMovie.release_date }}</p>
+              <p>{{ selectMovie.popularity }}</p>
+              <p>{{ selectMovie.vote_count }}</p>
+              <p>{{ selectMovie.vote_average }}</p>
+              <p>{{ selectMovie.poster_path }}</p>
+              <p>{{ selectMovie.tmdb_id }}</p>
+            </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           </div>
